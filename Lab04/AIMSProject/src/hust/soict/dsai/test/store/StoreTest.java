846b9a32c69e957db2bbfdf924bf92a4c699d70f
Phạm Tuấn Kiệt <111430161+kiet21042003@users.noreply.github.com>
2023-06-29 00:58:35 +0700

    Add files via upload

---
+++ Lab04/AIMSProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -0,0 +1,28 @@
+package hust.soict.dsai.test.store;
+import hust.soict.dsai.aims.store.*;
+import hust.soict.dsai.aims.media.*;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create a new store
+		Store myStore = new Store();
+		
+		//Create new dvd objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		myStore.addMedia(dvd1);
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		myStore.addMedia(dvd2);
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		myStore.addMedia(dvd3);
+		
+		//Remove dvd objects from the current cart
+		myStore.removeMedia(dvd1);
+
+		myStore.removeMedia(dvd3);	
+	}
+
+}
